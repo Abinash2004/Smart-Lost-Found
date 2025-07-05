dd78cbea68e54f2f30d0b7bac9a520860dc59ad8
Abinash Parida <abinashparida2021@gmail.com>
2025-07-05 10:23:31 +0530

    Home Redirect Fix

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-// src/routes/AppRoutes.jsx
+
 import { Routes, Route } from 'react-router-dom'
 import Dashboard from '../pages/Dashboard'
 import Login from '../pages/Login'
@@ -12,10 +12,12 @@ import MyFoundPosts from '../pages/MyFoundPosts'
 import ClaimPost from '../pages/ClaimPost'
 import ViewClaims from '../pages/ViewClaims'
 import MyClaims from '../pages/MyClaims'
+import HomeRedirect from '../pages/HomeRedirect'
 
 const AppRoutes = () => {
   return (
     <Routes>
+      <Route path="/" element={<HomeRedirect />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
